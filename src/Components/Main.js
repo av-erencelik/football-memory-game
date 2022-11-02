5493a0b8255a4df3927616c9844a212657af2ecf
Mehmet Eren Çelik <erenmemo05@gmail.com>
2022-11-02 22:01:18 +0300

    added animation to league selection

--- src/Components/Main.js
+++ src/Components/Main.js
@@ -2,26 +2,64 @@ import pl from "../Images/Leagues/pl.png";
 import tsl from "../Images/Leagues/tsl.png";
 import serieA from "../Images/Leagues/seriea.png";
 import bundes from "../Images/Leagues/bundesliga.webp";
+import { AnimatePresence, motion } from "framer-motion";
+import { useState } from "react";
+
 export function Main() {
+  const [isRender, setIsRender] = useState(true);
+
+  function handleClick() {
+    setIsRender(false);
+  }
   return (
     <main className="main">
-      <div className="level-choose">
-        <h2 className="instruction">Please, Choose A League To Play!</h2>
-        <div className="level-buttons">
-          <div className="level-button">
-            <img src={pl} alt="pl" className="league pl"></img>
-          </div>
-          <div className="level-button">
-            <img src={tsl} alt="tsl" className="league tsl"></img>
-          </div>
-          <div className="level-button">
-            <img src={serieA} alt="serieA" className="league serieA"></img>
-          </div>
-          <div className="level-button">
-            <img src={bundes} alt="bundes" className="league bundes"></img>
-          </div>
-        </div>
-      </div>
+      <AnimatePresence>
+        {isRender && (
+          <motion.div
+            className="level-choose"
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            transition={{ duration: 1.5 }}
+            exit={{ x: -1500, opacity: 0 }}
+          >
+            <h2 className="instruction">Please, Choose A League To Play!</h2>
+            <div className="level-buttons">
+              <div className="level-button">
+                <img
+                  src={pl}
+                  alt="pl"
+                  className="league pl"
+                  onClick={handleClick}
+                ></img>
+              </div>
+              <div className="level-button">
+                <img
+                  src={tsl}
+                  alt="tsl"
+                  className="league tsl"
+                  onClick={handleClick}
+                ></img>
+              </div>
+              <div className="level-button">
+                <img
+                  src={serieA}
+                  alt="serieA"
+                  className="league serieA"
+                  onClick={handleClick}
+                ></img>
+              </div>
+              <div className="level-button">
+                <img
+                  src={bundes}
+                  alt="bundes"
+                  className="league bundes"
+                  onClick={handleClick}
+                ></img>
+              </div>
+            </div>
+          </motion.div>
+        )}
+      </AnimatePresence>
     </main>
   );
 }
