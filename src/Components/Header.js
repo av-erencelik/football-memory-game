6ab5b2f6102f2d4d4633d8045042bb79d3e869c4
Mehmet Eren Çelik <erenmemo05@gmail.com>
2022-11-02 11:39:16 +0300

    changed 'A's with ball icon

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,7 +1,13 @@
+import { IoIosFootball } from "react-icons/io";
+
 export default function Header() {
   return (
     <header className="header">
-      <h1 className="title">MEMORYBALL</h1>
+      <h1 className="title">
+        MEMORYB
+        <IoIosFootball className="title-icon" />
+        LL
+      </h1>
     </header>
   );
 }
