8e814cdc75dd275a9273e17fd9d7bb720b89ff2e
Mehmet Eren Çelik <erenmemo05@gmail.com>
2022-11-02 22:54:58 +0300

    added animation to title

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,13 +1,22 @@
+import { motion } from "framer-motion";
 import { IoIosFootball } from "react-icons/io";
 
 export default function Header() {
   return (
     <header className="header">
-      <h1 className="title">
+      <motion.h1
+        className="title"
+        initial={{ y: -500 }}
+        animate={{ y: [-1500, 25, 0] }}
+        transition={{
+          duration: 1.2,
+          ease: "easeOut",
+        }}
+      >
         MEMORYB
         <IoIosFootball className="title-icon" />
         LL
-      </h1>
+      </motion.h1>
     </header>
   );
 }
