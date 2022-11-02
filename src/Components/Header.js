37349fa4bad20a630f9f6db728e469249df2c473
Mehmet Eren Çelik <erenmemo05@gmail.com>
2022-11-02 23:29:06 +0300

    add song and play stop button

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,9 +1,11 @@
 import { motion } from "framer-motion";
 import { IoIosFootball } from "react-icons/io";
+import { PlaySound } from "./PlaySound";
 
 export default function Header() {
   return (
     <header className="header">
+      <div></div>
       <motion.h1
         className="title"
         initial={{ y: -500 }}
@@ -17,6 +19,7 @@ export default function Header() {
         <IoIosFootball className="title-icon" />
         LL
       </motion.h1>
+      <PlaySound />
     </header>
   );
 }
---
+++ src/Components/PlaySound.js
@@ -0,0 +1,23 @@
+import menuSong from "../audio/menusong.mp3";
+import Sound from "react-sound";
+import { useState } from "react";
+import { GiSoundOn, GiSoundOff } from "react-icons/gi";
+
+export function PlaySound() {
+  const [willPlay, setWillPlay] = useState(false);
+  function handleClick() {
+    willPlay ? setWillPlay(false) : setWillPlay(true);
+  }
+  return (
+    <>
+      <Sound
+        url={menuSong}
+        playStatus={willPlay ? Sound.status.PLAYING : Sound.status.STOPPED}
+        playFromPosition={0}
+      />
+      <button onClick={handleClick} className="sound">
+        {willPlay ? <GiSoundOff /> : <GiSoundOn />}
+      </button>
+    </>
+  );
+}
