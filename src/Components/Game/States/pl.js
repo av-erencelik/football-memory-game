29bcafb97cb8ad1cd93f719fe9c602f026d9feac
Mehmet Eren Çelik <erenmemo05@gmail.com>
2022-11-03 17:23:16 +0300

    added seperate state file to declare

---
+++ src/Components/Game/States/pl.js
@@ -0,0 +1,64 @@
+import { v4 as uuidv4 } from "uuid";
+import arsenal from "../../../Images/PL/arsenal.png";
+import astonVilla from "../../../Images/PL/astonVilla.png";
+import bournemouth from "../../../Images/PL/bournemouth.png";
+import brentford from "../../../Images/PL/brentford.png";
+import brighton from "../../../Images/PL/brighton.png";
+import chelsea from "../../../Images/PL/chelsea.png";
+import crystal from "../../../Images/PL/crystal.png";
+import everton from "../../../Images/PL/everton.png";
+import fulham from "../../../Images/PL/fulham.png";
+import leeds from "../../../Images/PL/leeds.png";
+import leicester from "../../../Images/PL/leicester.png";
+import liverpool from "../../../Images/PL/liverpool.png";
+import manCity from "../../../Images/PL/manCity.png";
+import manUnited from "../../../Images/PL/manUnited.png";
+import newcastle from "../../../Images/PL/newcastle.png";
+import notForest from "../../../Images/PL/notForest.png";
+import southampton from "../../../Images/PL/southampton.png";
+import tottenham from "../../../Images/PL/tottenham.png";
+import westham from "../../../Images/PL/westham.png";
+import wolves from "../../../Images/PL/wolves.png";
+
+export const plState = [
+  { logo: arsenal, id: uuidv4(), isSelected: false, name: "arsenal" },
+  { logo: astonVilla, id: uuidv4(), isSelected: false, name: "aston villa" },
+  { logo: bournemouth, id: uuidv4(), isSelected: false, name: "bournemouth" },
+  { logo: brentford, id: uuidv4(), isSelected: false, name: "brentford" },
+  { logo: brighton, id: uuidv4(), isSelected: false, name: "brighton" },
+  { logo: chelsea, id: uuidv4(), isSelected: false, name: "chelsea" },
+  { logo: crystal, id: uuidv4(), isSelected: false, name: "crystal palace" },
+  { logo: everton, id: uuidv4(), isSelected: false, name: "everton" },
+  { logo: fulham, id: uuidv4(), isSelected: false, name: "fulham" },
+  { logo: leeds, id: uuidv4(), isSelected: false, name: "leeds united" },
+  {
+    logo: leicester,
+    id: uuidv4(),
+    isSelected: false,
+    name: "leicester city",
+  },
+  { logo: liverpool, id: uuidv4(), isSelected: false, name: "liverpool" },
+  { logo: manCity, id: uuidv4(), isSelected: false, name: "manchester city" },
+  {
+    logo: manUnited,
+    id: uuidv4(),
+    isSelected: false,
+    name: "manchester united",
+  },
+  {
+    logo: newcastle,
+    id: uuidv4(),
+    isSelected: false,
+    name: "newcastle united",
+  },
+  {
+    logo: notForest,
+    id: uuidv4(),
+    isSelected: false,
+    name: "nottingham forest",
+  },
+  { logo: southampton, id: uuidv4(), isSelected: false, name: "southampton" },
+  { logo: tottenham, id: uuidv4(), isSelected: false, name: "tottenham" },
+  { logo: westham, id: uuidv4(), isSelected: false, name: "west ham united" },
+  { logo: wolves, id: uuidv4(), isSelected: false, name: "wolves" },
+];
